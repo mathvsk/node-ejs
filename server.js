30e483783907b0dc9f30ea54f875060c63a928f6
Matheus Viscki <matheusviscki@gmail.com>
2022-01-27 10:24:07 -0300

    .

--- server.js
+++ server.js
@@ -3,8 +3,36 @@ const app = express()
 
 app.set("view engine", "ejs")
 
-app.get("/", function (rreq, res){
-  res.render("index")
+app.get("/", function (req, res){
+  const itens = [
+    { title: "D",
+      message: "esenvovler app de forma fácil"},
+    
+    { title: "E",
+      message: "JS usa JS para renderizar HTML"},
+
+    { title: "M",
+      message: "uito fácil de usar"},
+
+    { title: "A",
+      message: "mrozinho"},
+
+    { title : "I",
+      message: "nstall EJS"},
+
+    { title: "S",
+      message: "eja Foda"}
+  ]
+
+  const subtitle = "Uma linguagem de modelagem para criação de página HTML utilizando JS"
+  res.render("pages/index", {
+    qualidades: itens,
+    subtitle: subtitle
+  })
+})
+
+app.get("/sobre", function (req, res){
+  res.render("pages/sobre")
 })
 
 app.listen(8080)
